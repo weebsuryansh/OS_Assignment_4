5cc04b03bc6283c1fadf29128c538a78842dd43e
weebsuryansh <118445319+weebsuryansh@users.noreply.github.com>
2023-11-05 22:01:09 +0530

    Update sum.c

--- program-files/sum.c
+++ program-files/sum.c
@@ -1,9 +1,7 @@
 #define SIZE 1024
-int __attribute__((aligned(4096))) A[SIZE] = { 0 };
-int sum = 0;
 int _start() {
-  for (int i = 0; i < SIZE; i++) A[i] = 2;
+  int sum = 0;
   for (int i = 0; i < SIZE; i++)
-    sum += A[i];
+    sum += 2;
   return sum;
 }
